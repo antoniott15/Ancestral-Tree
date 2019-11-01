f0b8bc4f91dc8452e0a1a260100fa9cc91722eca
Antonio <antonio.toche@utec.edu.pe>
2019-11-01 16:54:36 -0500

    first addings

---
+++ estructure.hpp
@@ -0,0 +1,29 @@
+#ifndef ESTRUCTURE
+#define ESTRUCTURE
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string>
+
+using namespace std;
+typedef struct Parentesc
+{
+    typedef enum { mon, dad} relationship;
+    int generation;
+
+    void setGeneration(int g) {
+      generation = g;
+    }
+
+    int getGeneration() {
+      return generation;
+    }
+
+} parent;
+
+
+
+
+#endif
